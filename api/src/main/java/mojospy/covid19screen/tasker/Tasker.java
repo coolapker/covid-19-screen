827610633764f2eb559dfdd859f8d5197d5168a1
libm <coolapker@qq.com>
2023-01-28 11:17:13 +0800

    rename package

--- api/src/main/java/mojospy/covid19screen/tasker/Tasker.java
+++ api/src/main/java/mojospy/covid19screen/tasker/Tasker.java
@@ -1,8 +1,8 @@
 package mojospy.covid19screen.tasker;
 
-import mojospy.covid19screen.utils.update.UpdateDaily;
-import mojospy.covid19screen.utils.update.UpdateOverall;
-import mojospy.covid19screen.utils.update.UpdateProvinceCity;
+import mojospy.covid19screen.util.update.UpdateDaily;
+import mojospy.covid19screen.util.update.UpdateOverall;
+import mojospy.covid19screen.util.update.UpdateProvinceCity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,18 +10,18 @@ import javax.annotation.Resource;
 
 @Component
 public class Tasker {
-    @Resource
-    UpdateProvinceCity updateProvinceCity;
-    @Resource
-    UpdateOverall updateOverall;
-    @Resource
-    UpdateDaily updateDaily;
+	@Resource
+	UpdateProvinceCity updateProvinceCity;
+	@Resource
+	UpdateOverall updateOverall;
+	@Resource
+	UpdateDaily updateDaily;
 
-    //每天上午10:00点、下午14:00以及下午16:00执行任务
-    @Scheduled(cron = "0 0 10,14,16 * * ?")
-    public void update() {
-        updateProvinceCity.update();
-        updateOverall.update();
-        updateDaily.update();
-    }
+	//每天上午10:00点、下午14:00以及下午16:00执行任务
+	@Scheduled(cron = "0 0 10,14,16 * * ?")
+	public void update() {
+		updateProvinceCity.update();
+		updateOverall.update();
+		updateDaily.update();
+	}
 }
---
+++ api/src/main/java/mojospy/covid19screen/util/update/UpdateDaily.java
@@ -0,0 +1,52 @@
+package mojospy.covid19screen.util.update;
+
+import mojospy.covid19screen.domain.Daily;
+import mojospy.covid19screen.service.IDailyService;
+import mojospy.covid19screen.util.Spider;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class UpdateDaily {
+	@Resource
+	IDailyService dailyService;
+
+	@PostConstruct
+	public void update() {
+		List<Map> dailyList = Spider.getDaily();
+		for (Map day : dailyList) {
+			String dateStr = (String) day.get("date");
+			LocalDate date = LocalDate.parse(dateStr);
+
+			Map<String, Integer> total = (Map) day.get("total");
+			// 累计确诊
+			Integer confirm = total.get("confirm");
+			// 累计治愈
+			Integer heal = total.get("heal");
+
+			Daily daily = new Daily();
+			daily.setDateOf(date);
+			daily.setConfirmedCount(confirm);
+			daily.setCuredCount(heal);
+
+			Map<String, Integer> today = (Map) day.get("today");
+			// 现有确诊新增
+			Integer confirmIncr = today.get("confirm");
+			// 治愈新增
+			Integer healIncr = today.get("heal");
+			// 境外输入新增
+			Integer inputIncr = today.get("input");
+
+			daily.setConfirmedIncr(confirmIncr);
+			daily.setInputIncr(inputIncr);
+			daily.setCuredIncr(healIncr);
+
+			dailyService.saveOrUpdate(daily);
+		}
+	}
+}
